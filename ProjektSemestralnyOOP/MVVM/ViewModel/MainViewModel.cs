d44cf1ea910c28a2539b496eb85e084b187392f2
oiskra <ojaskra@gmail.com>
2022-05-10 18:10:04 +0200

    Login and Register buttons

--- ProjektSemestralnyOOP/MVVM/ViewModel/MainViewModel.cs
+++ ProjektSemestralnyOOP/MVVM/ViewModel/MainViewModel.cs
@@ -1,20 +1,38 @@
-﻿using System.ComponentModel;
+﻿using ProjektSemestralnyOOP.Commands;
+using System;
+using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
+using ProjektSemestralnyOOP.Interfaces;
+using ProjektSemestralnyOOP.Services;
+using System.Windows;
 
 namespace ProjektSemestralnyOOP.MVVM.ViewModel
 {
     public class MainViewModel : INotifyPropertyChanged
     {
 
+        private object _currentView;
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public StartUpViewModel StartUpVM { get; set; }
 
         public event PropertyChangedEventHandler PropertyChanged;
-        public object CurrentView { get; set; }
-
 
         public MainViewModel()
         {
-            CurrentView = new StartUpViewModel();
+            StartUpVM = new StartUpViewModel();
+            CurrentView = StartUpVM;
         }
+
 
         private void OnPropertyChanged([CallerMemberName]string propertyName = null)
         {
--- ProjektSemestralnyOOP/MVVM/ViewModel/StartUpViewModel.cs
+++ ProjektSemestralnyOOP/MVVM/ViewModel/StartUpViewModel.cs
@@ -1,14 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ProjektSemestralnyOOP.Commands;
+using System.Windows;
+using System.Windows.Input;
 
 namespace ProjektSemestralnyOOP.MVVM.ViewModel
 {
     public class StartUpViewModel
     {
+        public ICommand LoginButton { get; }
+        public ICommand RegisterButton { get; }
 
+        public StartUpViewModel()
+        {
+            LoginButton = new RelayCommand(LoginCommand);
+            RegisterButton = new RelayCommand(RegisterCommand);
+        }
+
+        private void RegisterCommand()
+        {
+            Window window = new RegisterWindow();
+            window.ShowDialog();
+        }
+
+        private void LoginCommand()
+        {
+            Window window = new LoginWindow();
+            window.ShowDialog();
+        }
     }
 }
